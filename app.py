0e4987ca8c8f5d1e9f7bab7079be529e3c4e24ae
Haloys <aloys.mangin@epitech.eu>
2024-10-01 14:56:30 +0200

    chore: remove everything for testing

--- app.py
+++ app.py
@@ -1,45 +1,9 @@
 from flask import Flask, send_from_directory, redirect, url_for
 from flask_cors import CORS
-import secrets
-from routes.auth import auth_bp
-from routes.fertilizer import fertilizer_bp
-from routes.fertilizer_cost_data import fertilizer_cost_bp
-from routes.agrochemical import agrochemical_bp
-from routes.agrochemical_cost_data import agrochemical_cost_bp
-from routes.seedling_cost_data import seedling_cost_bp
-from routes.labor_cost_data import labor_cost_bp
-from routes.unit_conversion_data import unit_conversion_bp
-from routes.origin_economics_data import origin_economics_bp
-from routes.survey_master_data import survey_master_bp
-from routes.real_time_info import real_time_info_bp
-from routes.status import status_bp
-
 app = Flask(__name__, static_folder='static')
 
 # Enable Cross-Origin Resource Sharing (CORS)
 CORS(app, supports_credentials=True)
-
-# Generate a secret key for the application
-app.secret_key = secrets.token_hex(16)
-
-# Register blueprints for different routes
-app.register_blueprint(auth_bp)
-app.register_blueprint(fertilizer_bp)
-app.register_blueprint(fertilizer_cost_bp)
-app.register_blueprint(agrochemical_bp)
-app.register_blueprint(agrochemical_cost_bp)
-app.register_blueprint(seedling_cost_bp)
-app.register_blueprint(labor_cost_bp)
-app.register_blueprint(unit_conversion_bp)
-app.register_blueprint(origin_economics_bp)
-app.register_blueprint(survey_master_bp)
-app.register_blueprint(real_time_info_bp)
-app.register_blueprint(status_bp)
-
-# Route to serve static files (CSS, JS, images)
-@app.route('/static/<path:path>')
-def serve_static_files(path):
-    return send_from_directory('static', path)
 
 # Serve the index.html at the /login URL for frontend
 @app.route('/login', methods=['GET'])
