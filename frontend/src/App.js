578e6b2f368117d99f7e7cebda8664de68f1f7bb
Haloys <aloys.mangin@epitech.eu>
2024-10-01 17:01:21 +0200

    fix: comment non use routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect, useHistory } from 'react-router-dom';
-import Fertilizer from './Fertilizer';
-import FertilizerCostData from './FertilizerCostData';
-import Agrochemical from './Agrochemical';
-import AgrochemicalCostData from './AgrochemicalCostData';
-import SeedlingCostData from './SeedlingCostData';
-import LaborCostData from './LaborCostData';
-import UnitConversionData from './UnitConversionData';
-import OriginEconomicsData from './OriginEconomicsData';
-import SurveyMasterData from './SurveyMasterData';
-import LoginForm from './LoginForm';
-import AdminPanel from './AdminPanel';
-import Status from './Status';
-import Home from './Home';
-import Navbar from './Navbar';
+//import Fertilizer from './Fertilizer';
+//import FertilizerCostData from './FertilizerCostData';
+//import Agrochemical from './Agrochemical';
+//import AgrochemicalCostData from './AgrochemicalCostData';
+//import SeedlingCostData from './SeedlingCostData';
+//import LaborCostData from './LaborCostData';
+//import UnitConversionData from './UnitConversionData';
+//import OriginEconomicsData from './OriginEconomicsData';
+//import SurveyMasterData from './SurveyMasterData';
+//import LoginForm from './LoginForm';
+//import AdminPanel from './AdminPanel';
+//import Status from './Status';
+//import Home from './Home';
+//import Navbar from './Navbar';
 import './App.css';
 import axios from 'axios';
 
@@ -50,39 +50,41 @@ function App() {
           <Route path="/login">
             {loggedIn ? <Redirect to="/" /> : <LoginForm onLogin={handleLogin} />} {/* Show LoginForm component if user is not logged in, otherwise redirect to home page */}
           </Route>
-          <Route path="/admin">
-            {loggedIn && isAdmin ? <AdminPanel /> : <Redirect to="/login" />} {/* Show AdminPanel component if user is logged in and is an admin, otherwise redirect to login page */}
+
+          {/* Commented routes for future use */}
+          {/* <Route path="/admin">
+            {loggedIn && isAdmin ? <AdminPanel /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/fertilizers_data">
-            {loggedIn ? <Fertilizer /> : <Redirect to="/login" />} {/* Show Fertilizer component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <Fertilizer /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/agrochemical_data">
-            {loggedIn ? <Agrochemical /> : <Redirect to="/login" />} {/* Show Agrochemical component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <Agrochemical /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/fertlizers_cost_data">
-            {loggedIn ? <FertilizerCostData /> : <Redirect to="/login" />} {/* Show FertilizerCostData component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <FertilizerCostData /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/agrochemical_cost_data">
-            {loggedIn ? <AgrochemicalCostData /> : <Redirect to="/login" />} {/* Show AgrochemicalCostData component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <AgrochemicalCostData /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/seedling_cost_data">
-            {loggedIn ? <SeedlingCostData /> : <Redirect to="/login" />} {/* Show SeedlingCostData component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <SeedlingCostData /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/labor_cost_data">
-            {loggedIn ? <LaborCostData /> : <Redirect to="/login" />} {/* Show LaborCostData component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <LaborCostData /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/unit_conversion_data">
-            {loggedIn ? <UnitConversionData /> : <Redirect to="/login" />} {/* Show UnitConversionData component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <UnitConversionData /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/origin_economics_data">
-            {loggedIn ? <OriginEconomicsData /> : <Redirect to="/login" />} {/* Show OriginEconomicsData component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <OriginEconomicsData /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/survey_master_data">
-            {loggedIn ? <SurveyMasterData /> : <Redirect to="/login" />} {/* Show SurveyMasterData component if user is logged in, otherwise redirect to login page */}
+            {loggedIn ? <SurveyMasterData /> : <Redirect to="/login" />} 
           </Route>
           <Route path="/status">
-            {loggedIn ? <Status /> : <Redirect to="/login" />} {/* Show Status component if user is logged in, otherwise redirect to login page */}
-          </Route>
+            {loggedIn ? <Status /> : <Redirect to="/login" />} 
+          </Route> */}
         </Switch>
       </div>
     </Router>
